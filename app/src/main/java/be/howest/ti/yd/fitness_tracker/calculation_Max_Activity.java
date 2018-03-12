693928a4887049f5ae624765556b1eb0abcec7ad
YanniD <yanni.derous@student.howest.be>
2018-03-12 14:46:39 +0100

    test
    test

--- app/src/main/java/be/howest/ti/yd/fitness_tracker/calculation_Max_Activity.java
+++ app/src/main/java/be/howest/ti/yd/fitness_tracker/calculation_Max_Activity.java
@@ -12,8 +12,11 @@ public class calculation_Max_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculation__max_);
         // todo put in a function
-        TextView tv_result = (TextView) findViewById(R.id.tv_result);
+        initResult();
+    }
 
+    public void initResult(){
+        TextView tv_result = (TextView) findViewById(R.id.tv_result);
         Intent mIntent = getIntent();
         double maxWeight = mIntent.getDoubleExtra("maxWeight", 0);
         tv_result.setText("The result is: " + maxWeight);
